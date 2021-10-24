f777a63c44ff480bc136032f8d2bbf959e178662
Pierre VIGIER <pierre.vigier@gmail.com>
2021-10-24 18:05:51 +0800

    Add spdlog to expine

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "SandBox/SandBox.h"
 
+#include "spdlog/spdlog.h"
+// #include "glm/glm.hpp"
+
 int main(int argc, char** argv) {
+    spdlog::info("Welcome to spdlog!");
+    spdlog::debug("This is a default");
     auto app = new SandBox("Test");
     app->Run();
     delete app;
