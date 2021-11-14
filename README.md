71cfd9abfb3dc5def3f36cea4ac503aed2b2846e
Pierre VIGIER <pierre.vigier@gmail.com>
2021-11-14 12:23:08 +0100

    Cpmpat hint to run on MacOS

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Expine
 ---
 
 Experiments with game engine, C++ and cmake
+
+
+On MacOS, clang seems to work better for debug mode
--- src/Expine/Application.cpp
+++ src/Expine/Application.cpp
@@ -26,7 +26,7 @@ namespace Expine
 
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         m_Window = glfwCreateWindow(960, 540, m_Title.c_str(), nullptr, nullptr);
